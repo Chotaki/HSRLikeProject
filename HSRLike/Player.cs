e548ccec20b07ebd06f94bd620e6d3182ff7bf46
frichard <frichard@gaming.tech>
2024-03-20 08:11:25 +0100

    Player v0.1

---
+++ HSRLike/Player.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace HsrLikeProject
+{
+    internal class Player
+    {
+
+        PlayerTeam()
+        {
+
+            void displayTeam(Team)
+            {
+                foreach (var team in playerTeam)
+                {
+                    Console.WriteLine(team.name);
+                }
+            }
+
+        }
+
+
+        public ItemList()
+        {
+
+            var Inventory = new List<int>();
+
+            void AddItem(Item.id)
+            {
+                Inventory.Add(Item);
+            }
+
+            void displayInventory()
+            {
+                foreach (var item in Inventory)
+                {
+                    Console.WriteLine(item.name);
+                }
+            }
+
+        }
+
+
+        public bool interact()
+        {
+            if (NPC)
+            {
+                if (NPC.boss == True)
+                {
+                    starFight();
+                }
+
+                startDialogue();
+            }
+
+            if (Chest)
+            {
+                open();
+            }
+
+            return true;
+        }
+
+        bool inFight()
+        {
+
+        }
+
+    }
+
+
+
+}
