a66f8a4d8e57b48e44754dc483ac9bc9b64ff2f2
frichard <frichard@gaming.tech>
2024-03-21 16:01:16 +0100

    pull Map v0.2

--- HSRLike/Map.cs
+++ HSRLike/Map.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HSRLike
+
+namespace HSRLikeProject
 {
     public class Map
     {
-
-        static int height = 32;
-        static int width = 128;
+        static int height = Console.WindowHeight ;
+        static int width = Console.WindowWidth;
+        static int borderW = 3;
 
         public static Map[,] map = new Map[height, width];
+
 
         public char character { get; set; }
 
@@ -21,6 +24,7 @@ namespace HSRLike
 
         public static void FillMap(Map tile)
         {
+
             //ligne
             for (int row = 0; row < height; row++)
             {
@@ -32,6 +36,45 @@ namespace HSRLike
             }
 
         }
+
+        // Displays the map to the player
+        public static void DisplayMap()
+        {
+            // Clear the console so we can redisplay the map
+            Console.Clear();
+            //ligne
+            for (int row = 0; row < height; row++)
+            {
+                //colonne
+                for (int col = 0; col < width; col++)
+                {
+ 
+                    //couleur
+                    Console.ForegroundColor = map[row, col].colour;
+                    //tile
+                    Console.Write(map[row, col].character);
+
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        // Create rectangle on the map with the maptile
+        public static void CreateRectangle(Point startPoint, int width, int height, Map tile)
+        {
+            // Starting from the point we specified, create a rectangle of the given map tile
+            for (int row = startPoint.X; row < startPoint.X + width; row++)
+            {
+                // For all columns
+                for (int col = startPoint.Y; col < startPoint.Y + height; col++)
+                {
+                    // Make that position the same as the tile passed in
+                    map[row, col] = tile;
+                }
+            }
+        }
+
     }
 }
 
--- HSRLike/Program.cs
+++ HSRLike/Program.cs
@@ -1,6 +1,7 @@
-﻿using HSRLike;
+﻿using HSRLikeProject;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,14 +14,45 @@ namespace Game
     {
         static void Main(string[] args)
         {
+            bool game = true;
+           
+
             Map defaultTile = new Map();
             defaultTile.character = 'o';
             defaultTile.colour = ConsoleColor.Green;
 
+
             Map.FillMap(defaultTile);
 
-        }
+            Map playerCharacter = new Map();
+            playerCharacter.colour = ConsoleColor.Red;
+            playerCharacter.character = 'P';
 
+            Point playerLocation = new Point(5,5);
+
+            //playerCharacter[playerLocation.X, playerLocation.Y] = defaultTile;
+
+
+            // Set up our water tile
+            Map waterTile = new Map();
+            waterTile.character = '~';
+            waterTile.colour = ConsoleColor.Blue;
+
+            // Create a little lake
+            Map.CreateRectangle(new Point(10, 10), 10, 10, waterTile);
+
+            Map.DisplayMap();
+
+            while (game)
+            {
+                Console.CursorVisible = false;
+                ConsoleKeyInfo input = Console.ReadKey(true);
+
+                Map.DisplayMap();
+
+
+            }
+        }
     }
 }
 
